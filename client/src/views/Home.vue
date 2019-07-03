1a2e982c1312022aed289556acfe62731359d110
Kharis Khasburrahman <khasburrahman@yahoo.com>
2019-07-03 10:29:00 +0700

    client notify if theres currently no product at the moment

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,11 +1,14 @@
 <template>
   <div class="home">
     <b-container class="bv-example-row">
-      <b-row>
-        <b-col v-for="(prod) in $store.state.products" :key="prod._id" sm="12" md="6" lg="4">
+      <b-row v-if="products.length > 0">
+        <b-col v-for="(prod) in products" :key="prod._id" sm="12" md="6" lg="4">
           <ProductCart v-bind="prod"/> 
         </b-col>
       </b-row>
+      <div class="align-center m-5" v-else>
+        <h3> No products </h3>
+      </div>
     </b-container>
   </div>
 </template>
@@ -13,8 +16,13 @@
 <script>
 // @ is an alias to /src
 import ProductCart from '@/components/CardProduct.vue'
+import { mapState } from 'vuex'
+
 export default {
   name: "home",
+  computed: mapState([
+    'products'
+  ]),
   data() {
     return {
       checkoutList: []
