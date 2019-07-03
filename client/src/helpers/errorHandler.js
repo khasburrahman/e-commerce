b329c0e967e592384ad6990cb49b12e9de6355aa
Kharis Khasburrahman <khasburrahman@yahoo.com>
2019-07-03 10:20:40 +0700

    update  client error handler

--- client/src/helpers/errorHandler.js
+++ client/src/helpers/errorHandler.js
@@ -2,7 +2,7 @@ const Toastify = require('./toastify')
 
 module.exports = function (err)  {
   if (err.isAxiosError) {
-    Toastify("Error: "+err.response.data)
+    Toastify("Error: "+(err.response && err.response.data || err.message))
   } else {
     console.error(err)
     Toastify("Programming Error - check console: "+err.message)
